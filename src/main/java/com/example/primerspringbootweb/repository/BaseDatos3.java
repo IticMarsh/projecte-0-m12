78f7d4282b703f563cf8cf1c67cda7f41e98e82d
IticMarsh <2023_arnau.marsino@iticbcn.cat>
2023-10-23 07:40:22 +0200

    eclipse

---
+++ src/main/java/com/example/primerspringbootweb/repository/BaseDatos3.java
@@ -0,0 +1,9 @@
+package com.example.primerspringbootweb.repository;
+
+
+import com.example.primerspringbootweb.bean.Libro;
+
+public interface BaseDatos3 extends JpaRepository<Libro, Integer> {
+	
+
+}
